86646b023324d67d88ddaa1315b525e779d0e0d1
Kibuchikenya <kibuchione@gmail.com>
2023-04-03 09:53:40 -0700

    0x07-pointers_arrays_strings

---
+++ 0x07-pointers_arrays_strings/0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* main - Test function for positive or negative
+* having fn coding
+* coded by robi
+*c is awesome
+*testing a function
+* Return: 0
+*/
+int main(void)
+{
+int i;
+i = 0;
+positive_or_negative(i);
+return (0);
+}
