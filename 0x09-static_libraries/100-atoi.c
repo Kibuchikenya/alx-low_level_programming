c06d2323bd6474ff3b5a16e1685ae62930d52feb
Kibuchikenya <kibuchione@gmail.com>
2023-04-09 06:33:21 -0700

    0x09-static_libraries

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,10 +1,9 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
+int _atoi(char *s)
 
-int _isdigit(int c)
 {
-    return isdigit(c);
+    return atoi(s);
 }
